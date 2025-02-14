e521de824c7b7c11633b6aa381922eb7e35c3cb7
Sabir675 <Elnareseferovaa@gmail.com>
2025-02-14 21:03:51 +0400

    Lesson29

---
+++ Backend2/src/Lesson29/Main.java
@@ -0,0 +1,14 @@
+package Lesson29;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person();
+		person.setName("Sabir");
+		person.setAge(12);
+		System.out.println(person.getName());
+		System.out.println(person.getAge());
+	}
+
+}
---
+++ Backend2/src/Lesson29/Person.java
@@ -0,0 +1,22 @@
+package Lesson29;
+
+public class Person {
+	String name;
+	int age;
+	
+	public String getName() {
+		return name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	void setAge(int age) {
+		this.age = age;
+	}
+}
